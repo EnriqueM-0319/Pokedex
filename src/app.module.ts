7f5487d14ed80cdeaa19618b8009f2e4be1787e0
EnriqueMendez19 <2005412@utrivieramaya.edu.mx>
2026-06-02 20:27:00 -0500

    refactor(database): switch mongoose module initialization to async

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EnvConfiguration } from './config/env.config';
 import { JoiValidationSchema } from './config/joi.validation';
 
@@ -21,8 +21,13 @@ import { JoiValidationSchema } from './config/joi.validation';
     // }),
     PokemonModule,
 
-    MongooseModule.forRoot(process.env.MONGODB || '', {
-      dbName: 'pokemonDb'
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('mongodb') || process.env.MONGODB,
+        dbName: 'pokemonDb',
+      }),
     }),
 
     CommonModule,
